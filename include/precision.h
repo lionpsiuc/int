d72f8272baffdbd89830ca241870f9c46ed7e7cb
Ion Lipsiuc <ionlipsiuc@gmail.com>
2025-05-08 14:20:00 +0100

    Add precision.h for precision control

---
+++ include/precision.h
@@ -0,0 +1,32 @@
+#ifndef PRECISION_H
+#define PRECISION_H
+
+#include <cfloat>
+#include <cmath>
+#include <limits>
+
+#ifdef DOUBLE
+typedef double PRECISION;
+#define ONE 1.0
+#define EULER 0.57721566490153286060
+#define EPSILON 1.E-30
+#define MAX_VAL DBL_MAX
+#define ABS(X) fabs(X)
+#define EXP(X) exp(X)
+#define LOG(X) log(X)
+#define TGAMMA(X) tgamma(X)
+#define POW(X, Y) pow(X, Y)
+#else
+typedef float PRECISION;
+#define ONE 1.0f
+#define EULER 0.5772156649f
+#define EPSILON 1.E-30f
+#define MAX_VAL FLT_MAX
+#define ABS(X) fabsf(X)
+#define EXP(X) expf(X)
+#define LOG(X) logf(X)
+#define TGAMMA(X) tgammaf(X)
+#define POW(X, Y) powf(X, Y)
+#endif
+
+#endif // PRECISION_H
