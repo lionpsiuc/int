3585f41de837d26a2895972e482d432a385d6628
Ion Lipsiuc <ionlipsiuc@gmail.com>
2025-05-08 16:55:00 +0100

    Add CPU timing utilities

---
+++ include/timing.h
@@ -0,0 +1,6 @@
+#ifndef TIMING_H
+#define TIMING_H
+
+double get_current_time();
+
+#endif // TIMING_H
---
+++ src/timing.cpp
@@ -0,0 +1,10 @@
+#include <cstddef>
+#include <sys/time.h>
+#include "timing_utils.h"
+
+double get_current_time() {
+  struct timeval current_time;
+  gettimeofday(&current_time, NULL);
+  return static_cast<double>(current_time.tv_sec) +
+         static_cast<double>(current_time.tv_usec) * 1e-6;
+}
